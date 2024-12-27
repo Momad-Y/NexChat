a9bd7a4bc2865efd7df416f048142a42c8b8c0f0
Momad-AI <Mohamed.y.abdelnasser@gmail.com>
2024-12-27 22:31:28 +0200

    Refactor summarization.py to use Hugging Face API for text summarization

--- src/nlp/summarization.py
+++ src/nlp/summarization.py
@@ -1,8 +1,56 @@
-from transformers import pipeline
+import requests
+from dotenv import dotenv_values, find_dotenv
+from typing import Generator
+import time
 
-# Initialize the summarization pipeline with a specific model
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+MAX_CHUNK_SIZE = 1000
+MAX_NEW_TOKENS = 50
 
-def summarize_text(text):
-    summary = summarizer(text, max_length=50, min_length=25, do_sample=False)
-    return summary[0]['summary_text']
+huggingface_api_key = dotenv_values(find_dotenv())["HUGGINGFACE_API_KEY"]
+headers = {"Authorization": f"Bearer {huggingface_api_key}"}
+
+
+def summarize_text(text: str) -> Generator[str, str, str]:
+    """
+    Summarizes the given text using an AI-powered summarization API.
+
+    Args:
+        text (str): The text to be summarized.
+
+    Returns:
+        str: The summarized text.
+    """
+    if len(text) > MAX_CHUNK_SIZE:
+        text_chunks = [
+            text[i : i + MAX_CHUNK_SIZE] for i in range(0, len(text), MAX_CHUNK_SIZE)
+        ]
+
+        for chunk in text_chunks:
+            payload = {
+                "inputs": chunk,
+                "parameters": {"max_new_tokens": MAX_NEW_TOKENS},
+            }
+            response = requests.post(API_URL, headers=headers, json=payload)
+            try:
+                yield response.json()[0]["summary_text"] + " "
+            except:
+                pass
+
+    else:
+        payload = {"inputs": text}
+        response = requests.post(API_URL, headers=headers, json=payload)
+        try:
+            summary = response.json()[0]["summary_text"]
+        except:
+            summary = "An error occurred while generating the summary."
+
+        # split the summary into sentences
+        sentences = summary.split(".")
+
+        # Remove empty strings
+        sentences = [sentence for sentence in sentences if sentence]
+
+        for sentence in sentences:
+            yield sentence.strip() + ". "
+            time.sleep(0.5)
